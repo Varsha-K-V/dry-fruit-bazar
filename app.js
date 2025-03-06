ae68fe01a10768e7f2e2f16a8521b46accab36f0
Varsha-K-V <varshakvas@gmail.com>
2025-03-06 20:51:08 +0530

    app file formatted

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
 const express = require("express");
 const app = express();
-const path=require("path");
+const path = require("path");
 const dotenv = require("dotenv").config();
-const session=require("express-session")
-const passport=require("./config/passport")
+const session = require("express-session");
+const passport = require("./config/passport");
 const db = require("./config/db");
-const userRouter=require("./routes/userRouter");
-const adminRouter=require("./routes/adminRouter");
-const flash=require('connect-flash')
-db()
+const userRouter = require("./routes/userRouter");
+const adminRouter = require("./routes/adminRouter");
+const flash = require("connect-flash");
+db();
 
-
-app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        secure:false,
-        httpOnly:true,
-        maxAge:72*60*60*1000
-    }
-}))
-
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 72 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.use(flash())
+app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req,res,next)=>{
-    res.set('cache-control','no-store');
-    next();
-})
+app.use((req, res, next) => {
+  res.set("cache-control", "no-store");
+  next();
+});
 
-app.set("view engine","ejs");
-app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
-app.use('/public',express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.set("views", [
+  path.join(__dirname, "views/user"),
+  path.join(__dirname, "views/admin"),
+]);
+app.use("/public", express.static(path.join(__dirname, "public")));
 
+app.use("/", userRouter);
+app.use("/admin", adminRouter);
 
-app.use("/",userRouter);
-app.use("/admin",adminRouter);
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server Running")
-}) 
-
+app.listen(process.env.PORT, () => {
+  console.log("Server Running");
+});
 
 module.exports = app;
